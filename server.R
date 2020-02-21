d008407807bb3c7b48806d42036d13dd2921caeb
Saltpile123 <ikhoudvanberichten@gmail.com>
2020-02-21 21:08:21 +0100

    LEKKER ZELF DOEN!

---
+++ server.R
@@ -0,0 +1,13 @@
+  ##########
+  # SERVER #
+  ##########
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+library(spotifyr)
+  
+function(input, output){
+  
+}
---
+++ ui.R
@@ -0,0 +1,15 @@
+  ######
+  # UI #
+  ######
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+library(spotifyr)
+
+navbarPage("Portfolio Computational Musicology",
+           tabPanel("Tab1",
+                    ),
+           tabPanel("Tab2",
+                    ))
